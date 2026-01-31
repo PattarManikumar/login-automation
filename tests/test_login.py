70ba5a9e4000c0831fcfe5d33853eadc40e8095a
PattarManikumar <manikumarpattar@gmail.com>
2026-01-31 20:56:49 +0530

    git commit

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,6 +4,6 @@ from pages.dashboard_page import DashboardPage
 def test_orangehrm_login(setup):
     login = LoginPage(setup)
     login.login("Admin", "admin123")
-
+    print("Hello")
     dashboard = DashboardPage(setup)
     assert dashboard.is_dashboard_displayed()
